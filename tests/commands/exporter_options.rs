820246025c5c0c1055d09c37d47c63ee12316876
Romain Lespinasse <romain.lespinasse@gmail.com>
2025-10-20 22:20:13 +0200

    cleanup

--- tests/commands/exporter_options.rs
+++ tests/commands/exporter_options.rs
@@ -1,26 +1,3 @@
-/*
-       // Drawio Desktop cli options for all formats
-       border
-       scale
-       enable-plugins
-       // Drawio Desktop cli options for PDF format
-       width
-       height
-       crop
-       embed-diagram
-       // Drawio Desktop cli options for PNG format
-       transparent
-       embed-diagram
-       // Drawio Desktop cli options for JPEG format
-       quality
-       // Drawio Desktop cli options for XML format
-       uncompressed
-       // Drawio Desktop cli options for SVG format
-       embed-svg-images
-       svg-theme
-       svg-links-target
-       embed-diagram
-*/
 use crate::DrawioExporterCommand;
 use anyhow::Result;
 use assert_cmd::prelude::*;
